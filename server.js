9632dfbf22354b3b78557dcbc6781adb20f6f7e4
gesyellopes <falecomgesyel@gmail.com>
2025-06-26 20:10:10 -0300

    Alterado a chave de ticket number

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post('/webhook/whatsapp', async (req, res) => {
         if (responseData.success) {
             await newTicket.update({
                 processed_file: responseData.file_id,
-                ticket_number: responseData.filename,
+                ticket_number: responseData.ticket_number,
                 processed_status: 'processed',
                 message_status: 'read'
             });
